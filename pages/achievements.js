2e2422d95a7cfd8279c8fe603c76cb22dc279b99
alasramus <77047027+alasramus@users.noreply.github.com>
2023-01-16 03:49:01 +0100

    Minor fixes

--- pages/achievements.js
+++ pages/achievements.js
@@ -4,9 +4,8 @@ import { prisma } from '../db'
 
 
 export default function Achievements({data}) {
-
-    {/* When achievement is unlocked */}
-    
+    let checkCompletion = arr => arr.every(v => v === true);
+    let complete = checkCompletion([data.achv_1, data.achv_2, data.achv_3])
     
     return (
           <div className='lg:h-screen p-5 bg-white rounded'>
@@ -46,7 +45,7 @@ export default function Achievements({data}) {
               <p className='text- mx-3 font-semibold text-slate-900 mb-5'>Cras iaculis ultricies nulla.</p>
             </div>
             
-            {/*Still not connected to */}
+            {/*Still not connected to db*/}
             <div 
               className={`m-14 w-72 h-72 h-200 bg-white rounded-lg shadow-md flex gap-4 flex-col items-center ${
                 data.achv_1 == true
@@ -79,8 +78,21 @@ export default function Achievements({data}) {
               <p className='text-lg font-black text-slate-900 mb-5'>Achievement 6</p>
               <p className='text- mx-3 font-semibold text-slate-900 mb-5'>Cras iaculis ultricies nulla.</p>
             </div>
+
+            <div 
+              className={`m-14 w-72 h-72 h-200 bg-white rounded-lg shadow-md flex gap-4 flex-col items-center ${
+                complete == true
+                ? "border-green-400 border-4"
+                : "border-grey-400 border-2"
+              }`}>
+              <img className='h-1/3 w-1/3 mt-6' src='https://cdn-icons-png.flaticon.com/512/2317/2317441.png'></img>
+              <p className='text-lg font-black text-slate-900 mb-5'>Ultimate Achievement</p>
+              <p className='text- mx-3 font-semibold text-slate-900 mb-5'>Complete all achievements.</p>
+            </div>
+            
           </div> 
         </div>   
+
     );
   }
 
