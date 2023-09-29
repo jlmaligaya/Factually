cccce7f6cdb4d5158854106013e7667d8633dd29
Jonas Louis Maligaya <77047027+jlmaligaya@users.noreply.github.com>
2023-09-29 14:27:26 +0200

    Update Activity 2

--- pages/activities/[activityID]/activity/2.js
+++ pages/activities/[activityID]/activity/2.js
@@ -290,33 +290,33 @@ const CaptchaGame = () => {
           <h2 className="text-6xl font-ogoby text-with-stroke mb-4">{calculatedScore}</h2>
         </div>
 
-  {/* Stars */}
-  <div className="flex items-center">
-    {Array.from({ length: 3 }, (_, index) => (
-      <div
-        key={index}
-        className={`w-16 h-16 ${
-          calculatedScore >= ((index + 1) / 3) * 100 ? 'fadeIn' : 'hidden'
-        } transition-opacity duration-1000`}
-      >
-        {/* Insert your star SVG or image here */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="yellow"
-          viewBox="0 0 24 24"
-          stroke-width="1"
-          stroke="black"
-          className="w-100 h-100"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.040.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
-          />
-        </svg>
-      </div>
-    ))}
-  </div>
+        {/* Stars */}
+        <div className="flex items-center">
+          {Array.from({ length: 3 }, (_, index) => (
+            <div
+              key={index}
+              className={`w-16 h-16 ${
+                calculatedScore >= ((index + 1) / 3) * 100 ? 'fadeIn' : 'hidden'
+              } transition-opacity duration-1000`}
+            >
+              {/* Insert your star SVG or image here */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="yellow"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="black"
+                className="w-100 h-100"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.040.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                />
+              </svg>
+            </div>
+          ))}
+        </div>
 
         <div className="flex w-full justify-center gap-4 pt-4">
           <button
