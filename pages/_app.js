51f0c3ab25bca8efad82101464b51a339d3f53de
Jonas Louis Maligaya <77047027+jlmaligaya@users.noreply.github.com>
2023-12-04 00:11:02 +0100

    Viewport update

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
-import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 import Layout from "../components/Layout";
-import { getSession } from 'next-auth/react';
+import { getSession } from "next-auth/react";
+import Head from "next/head";
+import "../styles/globals.css";
 
 export default function MyApp({
-   Component, 
-   pageProps: {session, ...pageProps} }) {
-  
-  // const getLayout = Component.getLayout || ((page) => page)
-  return <SessionProvider session={session}><Component {...pageProps} /></SessionProvider>
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Other head elements, like title, link to stylesheets, etc. */}
+      </Head>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
-
