775c2a6ef1809f203a7d6ac4ef464910bed6c6d5
Jonas Louis Maligaya <77047027+jlmaligaya@users.noreply.github.com>
2024-02-07 08:55:37 +0800

    Day 2 - Leaderboards Fix

--- pages/api/leaderboards.js
+++ pages/api/leaderboards.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       where: {
         activityId: activityID,
         user: {
-          section: "S1206",
+          section: sectionId,
         },
       },
       orderBy: [
