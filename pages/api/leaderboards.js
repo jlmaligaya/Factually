01c14b2c89413f4c471ddb097ffa8a3b2be54146
Jonas Louis Maligaya <77047027+jlmaligaya@users.noreply.github.com>
2024-02-16 09:31:32 +0800

    Leaderboard Instructor Page

--- pages/api/leaderboards.js
+++ pages/api/leaderboards.js
@@ -4,30 +4,57 @@ export default async function handler(req, res) {
   try {
     const activityID = req.query.activityID;
     const sectionId = req.query.sectionId;
-    const scoresWithUsername = await prisma.score.findMany({
-      where: {
-        activityId: activityID,
-        // user: {
-        //   section: sectionId,
-        // },
-      },
-      orderBy: [
-        { score: "desc" }, // Order by score in descending order
-        { timeFinished: "asc" }, // Then order by timeFinished in ascending order
-      ],
 
-      take: 10,
-      include: {
-        user: {
-          select: {
-            username: true, // Include the username field
-            avatar: true,
-            firstName: true,
-            lastName: true,
+    let scoresWithUsername;
+    if (sectionId === "faculty") {
+      scoresWithUsername = await prisma.score.findMany({
+        where: {
+          activityId: activityID,
+          NOT: { user: { role: "instructor" } },
+          // Omitted the user.section condition to fetch scores without section
+        },
+        orderBy: [
+          { score: "desc" }, // Order by score in descending order
+          { timeFinished: "asc" }, // Then order by timeFinished in ascending order
+        ],
+
+        include: {
+          user: {
+            select: {
+              username: true, // Include the username field
+              avatar: true,
+              firstName: true,
+              lastName: true,
+            },
           },
         },
-      },
-    });
+      });
+    } else {
+      scoresWithUsername = await prisma.score.findMany({
+        where: {
+          activityId: activityID,
+          user: {
+            section: sectionId,
+          },
+        },
+        orderBy: [
+          { score: "desc" }, // Order by score in descending order
+          { timeFinished: "asc" }, // Then order by timeFinished in ascending order
+        ],
+
+        take: 10,
+        include: {
+          user: {
+            select: {
+              username: true, // Include the username field
+              avatar: true,
+              firstName: true,
+              lastName: true,
+            },
+          },
+        },
+      });
+    }
 
     if (!scoresWithUsername) {
       res.status(404).json({ error: "Video not found" });
